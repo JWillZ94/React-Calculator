aeb1f854297b2fdfb46a9c17e1ce990c421f939c
Jerel Williams <jerel@users-MacBook-Pro.local>
2020-01-18 21:36:21 -0500

    Got it done, looks like I'll have to work on my creativity..

--- calculator/src/Calc/Output/Output.js
+++ calculator/src/Calc/Output/Output.js
@@ -5,24 +5,9 @@ class Output extends React.Component {
 
     render() {
 
-        let inp = <input id="output" value={this.props.screen} readOnly/>;
-
-        console.log(inp);
-
-        // function showCaret(input) {
-        //     console.log(in);
-        //     // let widthSizeRatio = 21.6/36;
-        //     // let charWidth = widthSizeRatio * 36;
-        //     // let length = input.value.length;
-        //     // let cur = input.selectionStart;
-        //     //
-        //     // document.getElementById("caret").style.right = Math.floor((length - cur) * charWidth) + "px";
-        // }
-
         return (
-            <div id="output-container">
-                {inp}
-                <span id="caret">|</span>
+            <div id="output">
+                <span id="output-value-space">{this.props.screen}</span>
             </div>
         );
     }
